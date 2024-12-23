994582a314d0195314cba30e343cf3e56ad08d84
antpie530 <antpie530@gmail.com>
2024-12-23 21:30:53 +0000

    Add Named Constants to Reverse Temp COnvertor Program

--- chapter_1/temp_convertor/reverse_temp_convertor.c
+++ chapter_1/temp_convertor/reverse_temp_convertor.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 
+#define LOWER 0
+#define UPPER 300
+#define STEP 20
+
 main() {
     int farh;
 
-    for (farh = 300; farh >= 0; farh = farh - 20) {
+    for (farh = UPPER; farh >= LOWER; farh = farh - STEP) {
         printf("%3d\t%6.1f\n", farh, (5.0 / 9.0) * (farh + 32));
     }
 }
