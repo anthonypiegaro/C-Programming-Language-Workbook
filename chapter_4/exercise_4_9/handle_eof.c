ec0b362dd07b760d52f94681fcf5a4227b79be3c
antpie530 <antpie530@gmail.com>
2025-01-02 19:16:55 +0000

    Add Exercise 4-9 Solution

---
+++ chapter_4/exercise_4_9/handle_eof.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+#define ARRAYSIZE 100
+
+int buf;
+char buf_valid;
+
+int getch(void);
+void ungetch(int);
+
+int main() {
+    int c, i;
+    char s[ARRAYSIZE];
+    i = 0;
+
+    ungetch(EOF);
+
+    while ((c = getch()) != EOF) {
+        if (c == '\n') {
+            s[i] = '\0';
+            printf("%s\n", s);
+            i = 0;
+        } else {
+            s[i++] = c;
+        }
+    }
+
+    return 0;
+}
+
+int getch(void) {
+    if (buf_valid) {
+        buf_valid = 0;
+        return buf;
+    } else {
+        return getchar();
+    }
+}
+
+void ungetch(int c) {
+    if (buf_valid)
+        printf("ungetch: buffer already in use\n");
+    else
+        buf_valid = 1;
+        buf = c;
+}
