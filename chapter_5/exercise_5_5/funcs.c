f05b4bb5b7d08cd1591ffcc77f2be92a348a51c8
antpie530 <antpie530@gmail.com>
2025-01-05 03:41:17 +0000

    Add strncat Function to Exercise 5-5

--- chapter_5/exercise_5_5/funcs.c
+++ chapter_5/exercise_5_5/funcs.c
@@ -9,10 +9,14 @@ int strncmp_(char *s, char *t, int n);
 
 int main() {
     char r[SIZE] = "Hello";
+    char s[SIZE] = "Right-handed";
 
     strncpy_(r, "one", 6);
 
+    strncat_(s, " Left-handed", 15);
+
     printf("%s\n", r);
+    printf("%s\n", s);
 
     return 0;
 }
@@ -23,4 +27,14 @@ void strncpy_(char *s, char *t, int n) {
     
     if (*t == '\0')
         *s = '\0';
+}
+
+void strncat_(char *s, char *t, int n) {
+    while (*s != '\0')
+        ++s;
+    
+    for (int i = 0; i < n && *t != '\0'; ++i)
+        *s++ = *t++;
+    
+    *s = '\0';
 }
