00aee807dfaf32ed89980002b0956423bf9f1cb8
antpie530 <antpie530@gmail.com>
2025-01-05 04:01:59 +0000

    Add Exercise 5-5 Solution

--- chapter_5/exercise_5_5/funcs.c
+++ chapter_5/exercise_5_5/funcs.c
@@ -1,27 +1,28 @@
 #include <stdio.h>
-#include <string.h>
 
 #define SIZE 100
 
-void strncpy_(char *s, char *t, int n);
-void strncat_(char *s, char *t, int n);
-int strncmp_(char *s, char *t, int n);
+void strncpy(char *s, char *t, int n);
+void strncat(char *s, char *t, int n);
+int strncmp(char *s, char *t, int n);
 
 int main() {
     char r[SIZE] = "Hello";
     char s[SIZE] = "Right-handed";
+    char t[SIZE] = "The Remote";
 
-    strncpy_(r, "one", 6);
+    strncpy(r, "one", 6);
 
-    strncat_(s, " Left-handed", 15);
+    strncat(s, " Left-handed", 15);
 
     printf("%s\n", r);
     printf("%s\n", s);
+    printf("%d\n", strncmp(t, "The Remote", 45));
 
     return 0;
 }
 
-void strncpy_(char *s, char *t, int n) {
+void strncpy(char *s, char *t, int n) {
     for (int i = 0; i < n && *t != '\0'; ++i)
         *s++ = *t++;
     
@@ -29,7 +30,7 @@ void strncpy_(char *s, char *t, int n) {
         *s = '\0';
 }
 
-void strncat_(char *s, char *t, int n) {
+void strncat(char *s, char *t, int n) {
     while (*s != '\0')
         ++s;
     
@@ -37,4 +38,30 @@ void strncat_(char *s, char *t, int n) {
         *s++ = *t++;
     
     *s = '\0';
+}
+
+int strncmp(char *s, char *t, int n) {
+    int i;
+
+    for (i = 0; i < n && *s != '\0' && *t != '\0'; ++i) {
+        if (*s < *t) {
+            return -1;
+        } else if (*s > *t) {
+            return 1;
+        }
+
+        ++s;
+        ++t;
+    }
+
+    if (i == n)
+        return 0;
+
+    if (*s < *t) {
+        return -1;
+    } else if (*s == *t) {
+        return 0;
+    } else {
+        return 1;
+    }
 }
